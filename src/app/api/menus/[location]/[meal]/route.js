ce4e6e9ffe3d5e6b1e3c3d3d6f5318026417863c
Phyopma <phyopyaemaung1@gmail.com>
2024-11-20 11:05:49 -0800

    commented api routes

--- src/app/api/menus/[location]/[meal]/route.js
+++ src/app/api/menus/[location]/[meal]/route.js
@@ -1,27 +1,27 @@
-import { mealParamsId } from "@/app/queryParams/mealParams";
-import { buildUrl, formatResponse } from "@/lib/helper";
+// import { mealParamsId } from "@/app/queryParams/mealParams";
+// import { buildUrl, formatResponse } from "@/lib/helper";
 
-export async function GET(request, { params }) {
-  const { location } = await params;
+// export async function GET(request, { params }) {
+//   const { location } = await params;
 
-  // lunch or brunch or dinner
-  const urls = [
-    buildUrl(location, mealParamsId["Lunch"]),
-    buildUrl(location, mealParamsId["Brunch"]),
-    buildUrl(location, mealParamsId["Dinner"]),
-  ];
-  const responses = await Promise.all(
-    urls.map((url) => fetch(url).then((res) => res.json()))
-  );
-  // const response = await fetch(url).then((res) => res.json());
-  const data = formatResponse(responses);
-  console.log(data);
+//   // lunch or brunch or dinner
+//   const urls = [
+//     buildUrl(location, mealParamsId["Lunch"]),
+//     buildUrl(location, mealParamsId["Brunch"]),
+//     buildUrl(location, mealParamsId["Dinner"]),
+//   ];
+//   const responses = await Promise.all(
+//     urls.map((url) => fetch(url).then((res) => res.json()))
+//   );
+//   // const response = await fetch(url).then((res) => res.json());
+//   const data = formatResponse(responses);
+//   console.log(data);
 
-  return Response.json({
-    status: 200,
-    body: data,
-  });
-}
+//   return Response.json({
+//     status: 200,
+//     body: data,
+//   });
+// }
 
 // Menu<Object> {
 //     locationId<String>,
